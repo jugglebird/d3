80e5a6a766f3b50428e4b54167fc223fbdf0d9b7
Chris Hunter <chunter@wondertwinpowers.net>
2011-10-06 11:56:00 -0500

    SVGWeb doesn't support setting the textContent of an existing node, so for SVG nodes in that case, we fall back to appending a new textNode with our value.

--- src/core/selection-text.js
+++ src/core/selection-text.js
@@ -1,6 +1,17 @@
 d3_selectionPrototype.text = function(value) {
-  return arguments.length < 1 ? this.node().textContent
-      : (this.each(typeof value === "function"
-      ? function() { this.textContent = value.apply(this, arguments); }
-      : function() { this.textContent = value; }));
+  if (this.node()._fakeNode) {
+    if (arguments.length < 1) {
+      return this.node().textContent;
+    } else {
+      return this.each(typeof value === "function"
+        ? function() { this.appendChild(document.createTextNode(value.apply(this, arguments), true)); }
+        : function() { this.appendChild(document.createTextNode(value, true)); }
+      );
+    }
+  } else {
+    return arguments.length < 1 ? this.node().textContent
+        : (this.each(typeof value === "function"
+        ? function() { this.textContent = value.apply(this, arguments); }
+        : function() { this.textContent = value; }));
+  }
 };
